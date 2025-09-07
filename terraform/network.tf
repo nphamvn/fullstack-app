166b2abfe5824cdaae972cc343619b22d2771ddd
Nam Pham <pvnam95@hotmail.com>
2025-09-07 14:04:55 +0900

    feat: add AWS VPC and subnets configuration in Terraform

---
+++ terraform/network.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "fullstack_app" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "fullstack_app"
+  }
+}
+
+resource "aws_subnet" "fullstack_app1" {
+  vpc_id                  = aws_vpc.fullstack_app.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "fullstack_app1"
+  }
+}
+
+resource "aws_subnet" "fullstack_app2" {
+  vpc_id                  = aws_vpc.fullstack_app.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "fullstack_app2"
+  }
+}
+
+resource "aws_subnet" "fullstack_app3" {
+  vpc_id                  = aws_vpc.fullstack_app.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-northeast-1d"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "fullstack_app3"
+  }
+}
+
