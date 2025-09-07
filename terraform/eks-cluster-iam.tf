282164294e8877862605e6368d3988cc9e8b7143
Nam Pham <pvnam95@hotmail.com>
2025-09-07 14:13:54 +0900

    feat: add IAM role and policy attachment for EKS cluster in Terraform

---
+++ terraform/eks-cluster-iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "EKSClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
